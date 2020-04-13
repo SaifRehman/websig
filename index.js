31cfad0d4bdaae251f330ae8934c39efaf193869
saif <s4saif.121@gmail.com>
2020-04-13 21:31:59 +0400

    init

---
+++ index.js
@@ -0,0 +1,135 @@
+const IPFS = require('ipfs')
+const Room = require('ipfs-pubsub-room')
+const Rx = require('rxjs')
+function websig(roomname, iceserver) {
+    this.room;
+    this.datachannel;
+    this.ispeerjoined = new Rx.BehaviorSubject(false);
+    this.pc = new RTCPeerConnection(iceserver);
+    this.ipfsid;
+    this.peer;
+    this.datachannelData = new Rx.BehaviorSubject(undefined)
+    this.ipfs = new IPFS({
+        repo: 'ipfs/pubsub-demo/' + Math.random(),
+        EXPERIMENTAL: {
+            pubsub: true
+        },
+        config: {
+            Addresses: {
+                Swarm: ['/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star']
+            }
+        }
+    });
+    this.roomname = roomname;
+    this.pc.onicecandidate = (event => {
+        if (event.candidate) {
+            this.sendMessage(JSON.stringify({
+                message: {
+                    'ice': event.candidate,
+                },
+                sender: this.ipfsid
+            }));
+        } else {
+            console.log('all ice has been sent');
+        }
+    });
+    const self = this;
+    this.datachannel = this.pc.createDataChannel('dataChannel', {
+        negotiated: true,
+        id: 0
+    });
+    this.datachannel.onclose = function (event) {
+        console.log(' data channel close', event);
+    };
+    this.datachannel.onerror = function (err) {
+        console.log(' data channel error', event);
+    };
+    this.datachannel.onmessage = function (event) {
+        
+        console.log(event.data)
+        self.datachannelData.next(event.data);
+    }
+    this.ipfs.once('ready', () => this.ipfs.id((err, info) => {
+        if (err) {
+            throw err
+        }
+        this.ipfsid = info.id
+        this.room = Room(this.ipfs, this.roomname)
+        this.room.on('peer joined', (peer) => {
+            console.log('peer ' + peer + ' joined')
+            this.peer = peer;
+
+            this.ispeerjoined.next(true);
+        })
+        this.room.on('peer left', (peer) => {
+            console.log('peer ' + peer + ' left');
+            this.ispeerjoined.next(false);
+        })
+        this.room.on('message', (message) => {
+            var bigmsg = JSON.parse(message.data.toString());
+            const msg = bigmsg.message;
+            const sender = bigmsg.sender;
+            console.log(sender, this.ipfsid, bigmsg)
+            if (sender !== this.ipfsid) {
+                if (msg.ice) {
+                    if (this.pc) {
+                        this.pc.addIceCandidate(new RTCIceCandidate(msg.ice));
+                    }
+                } else if (msg.sdp.type === 'offer') {
+                    this.pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+                        .then(() => this.pc.createAnswer())
+                        .then(answer => {
+                            return this.pc.setLocalDescription(answer);
+                        })
+                        .then(() => this.sendMessage(JSON.stringify({
+                            message: {
+                                sdp: this.pc.localDescription,
+                            },
+                            sender: this.ipfsid
+                        })));
+                } else if (msg.sdp.type === 'answer') {
+                    this.pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+                }
+            }
+        })
+    }))
+}
+websig.prototype.getipfsid = function () {
+    return this.ipfsid;
+}
+websig.prototype.setipfsid = function (getipfsid) {
+    this.ipfsid = getipfsid;
+}
+websig.prototype.getRoomname = function () {
+    return this.roomname;
+}
+
+websig.prototype.sendMessage = function (message) {
+    this.room.sendTo(this.peer, message)
+}
+
+websig.prototype.getIceserver = function () {
+    return this.iceserver;
+}
+websig.prototype.connectWEBRTC = function () {
+    if(this.ispeerjoined.value){
+        this.pc.createOffer()
+        .then(offer => this.pc.setLocalDescription(offer))
+        .then(() => {
+            this.sendMessage(JSON.stringify({
+                message: {
+                    sdp: this.pc.localDescription
+                },
+                sender: this.ipfsid,
+            }))
+        });
+    }
+}
+websig.prototype.send = function (msg) {
+    console.log(this.datachannel.readyState)
+    if (this.datachannel.readyState === 'open') {
+        this.datachannel.send(msg);
+    }
+}
+
+module.exports = websig;
